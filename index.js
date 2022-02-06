517a5b178f081645450fc062fb0169f9b0c1276f
Nasser Nasser <nhani.nasser@gmail.com>
2022-02-06 16:36:13 -0500

    set up folders and files and the constants.

---
+++ index.js
@@ -0,0 +1,7 @@
+// makes the new page
+const makeHTML = require('./src/generateHTML');
+
+// Team profiles
+const manager = require('./library/manager');
+const engineer = require('./library/engineer')
+const intern = require('./library/intern')
