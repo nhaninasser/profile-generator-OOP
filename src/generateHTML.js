5df80e205b8f12f15bae3faf451b556831b5a77b
Nasser Nasser <nhani.nasser@gmail.com>
2022-02-06 21:03:41 -0500

    making the html layout which will be generated as well as linking the style sheets

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -52,7 +52,7 @@ const generateIntern = function (intern) {
     </div>
 </div>
     `
-};
+}
 
 // create Intern card 
 const generateIntern = function (intern) {
@@ -105,3 +105,30 @@ generateHTML = (data) => {
         pageArray.push(internCard);
     }
 }
+
+
+// joining strings 
+const employeeCards = pageArray.join('')
+
+// return the page generated
+const generateTeam = generateTeamPage(employeeCards);
+return generateTeam;
+
+}
+
+// generate htmlPage
+
+const generateTeamPage = function (employeeCards) {
+    return`
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+  </head>
