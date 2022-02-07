7eb20891dcedb68c9d40f7b0986a0a0c450fb14e
Nasser Nasser <nhani.nasser@gmail.com>
2022-02-06 20:08:53 -0500

    added the function to gerate html

--- index.js
+++ index.js
@@ -194,3 +194,17 @@ const addEmployee = () => {
     })
     
 };
+
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+}; 
