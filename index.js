7fe3c768354abd3921913d2ba49efaa68e079d90
Nasser Nasser <nhani.nasser@gmail.com>
2022-02-06 20:10:35 -0500

    added call functions

--- index.js
+++ index.js
@@ -208,3 +208,15 @@ const writeFile = data => {
         }
     })
 }; 
+
+addManager()
+  .then(addEmployee)
+  .then(teamArray => {
+    return generateHTML(teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
